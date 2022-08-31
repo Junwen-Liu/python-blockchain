06636cc24821be81128ced86b06772bcf3a92f31
junwenliu <junwenliu7@gmail.com>
2022-08-31 10:51:33 +0800

    successfully connected to cloud redis server

--- backend/util/pubsub.py
+++ backend/util/pubsub.py
@@ -2,9 +2,9 @@ import redis
 
 def main():
     r = redis.Redis(host="123.60.55.61", port=6379,decode_responses=True)
-    #r.set("name","slkdfjafds")
+    r.set("name","slkdfjafds")
     #r.flushall()
-    r.delete('name')
+    #r.delete('name')
     print(r.get("name"))
 
 if __name__ == '__main__':
